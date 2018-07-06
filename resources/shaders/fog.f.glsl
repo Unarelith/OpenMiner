23cc904cf3a7edb8b7d29aedf1d5fc9fe457503d
Quentin Bazin <quent42340@gmail.com>
2018-07-06 17:47:31 +0200

    [fog.f.glsl] Small fix. Now using sky color instead of a transparent color.

--- resources/shaders/fog.f.glsl
+++ resources/shaders/fog.f.glsl
@@ -3,6 +3,6 @@
 vec4 fog(vec4 color, float fogCoord, float fogStart, float fogEnd) {
 	float fog = clamp((fogEnd - fogCoord) / (fogEnd - fogStart), 0.0, 1.0);
 
-	return mix(vec4(color.xyz, 0.0), color, fog);
+	return mix(vec4(0.196078, 0.6, 0.8, 1.0), color, fog);
 }
 
