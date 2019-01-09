c36481f4ef8bb0693e9c53984688e2f75d947f8d
Quentin Bazin <quent42340@gmail.com>
2019-01-09 19:32:28 +0100

    [CMakeLists.txt] Improved build for client/server architecture.

---
+++ server/source/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:
+ *
+ *        Created:  13/12/2014 20:49:00
+ *
+ *         Author:  Quentin Bazin, <quent42340@gmail.com>
+ *
+ * =====================================================================================
+ */
+#include "Application.hpp"
+
+int main(int argc, char **argv) {
+	Application app(argc, argv);
+	app.run();
+
+	return 0;
+}
+
