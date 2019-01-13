307a1bdb2357e8a8ebe880993c7afbb5ce0f980a
Quentin Bazin <quent42340@gmail.com>
2019-01-13 21:53:59 +0100

    Now using Faithful 32x texture pack for items.

--- mods/items.lua
+++ mods/items.lua
@@ -1,13 +1,15 @@
 mod:item {
 	id = "default:stick",
 	name = "Stick",
-	texture = 324,
+	-- texture = 324, -- Vanilla
+	texture = 331, -- Faithful 32x
 }
 
 mod:item {
 	id = "default:axe_stone",
 	name = "Stone Axe",
-	texture = 325,
+	-- texture = 325, -- Vanilla
+	texture = 332, -- Faithful 32x
 	mining_speed = 4,
 	harvest_capability = 4,
 }
@@ -15,13 +17,15 @@ mod:item {
 mod:item {
 	id = "default:hoe_stone",
 	name = "Stone Hoe",
-	texture = 326,
+	-- texture = 326, -- Vanilla
+	texture = 333, -- Faithful 32x
 }
 
 mod:item {
 	id = "default:pickaxe_stone",
 	name = "Stone Pickaxe",
-	texture = 327,
+	-- texture = 327, -- Vanilla
+	texture = 334, -- Faithful 32x
 	mining_speed = 4,
 	harvest_capability = 1,
 }
@@ -29,7 +33,8 @@ mod:item {
 mod:item {
 	id = "default:shovel_stone",
 	name = "Stone Shovel",
-	texture = 328,
+	-- texture = 328, -- Vanilla
+	texture = 335, -- Faithful 32x
 	mining_speed = 4,
 	harvest_capability = 2,
 }
@@ -37,13 +42,15 @@ mod:item {
 mod:item {
 	id = "default:sword_stone",
 	name = "Stone Sword",
-	texture = 329,
+	-- texture = 329, -- Vanilla
+	texture = 336, -- Faithful 32x
 }
 
 mod:item {
 	id = "default:coal",
 	name = "Coal",
-	texture = 111,
+	-- texture = 111, -- Vanilla
+	texture = 109, -- Faithful 32x
 	is_fuel = true,
 	burn_time = 1600,
 }
@@ -51,13 +58,15 @@ mod:item {
 mod:item {
 	id = "default:ingot_iron",
 	name = "Iron Ingot",
-	texture = 232,
+	-- texture = 232, -- Vanilla
+	texture = 229, -- Faithful 32x
 }
 
 mod:item {
 	id = "default:charcoal",
 	name = "Charcoal",
-	texture = 41,
+	-- texture = 41, -- Vanilla
+	texture = 40, -- Faithful 32x
 	is_fuel = true,
 	burn_time = 1600,
 }
@@ -65,7 +74,8 @@ mod:item {
 mod:item {
 	id = "default:axe_wood",
 	name = "Wooden Axe",
-	texture = 337,
+	-- texture = 337, -- Vanilla
+	texture = 353, -- Faithful 32x
 	mining_speed = 2,
 	harvest_capability = 4,
 }
@@ -73,13 +83,15 @@ mod:item {
 mod:item {
 	id = "default:hoe_wood",
 	name = "Wooden Hoe",
-	texture = 338,
+	-- texture = 338, -- Vanilla
+	texture = 354, -- Faithful 32x
 }
 
 mod:item {
 	id = "default:pickaxe_wood",
 	name = "Wooden Pickaxe",
-	texture = 339,
+	-- texture = 339, -- Vanilla
+	texture = 355, -- Faithful 32x
 	mining_speed = 2,
 	harvest_capability = 1,
 }
@@ -87,7 +99,8 @@ mod:item {
 mod:item {
 	id = "default:shovel_wood",
 	name = "Wooden Shovel",
-	texture = 340,
+	-- texture = 340, -- Vanilla
+	texture = 356, -- Faithful 32x
 	mining_speed = 2,
 	harvest_capability = 2,
 }
@@ -95,6 +108,7 @@ mod:item {
 mod:item {
 	id = "default:sword_wood",
 	name = "Wooden Sword",
-	texture = 341,
+	-- texture = 341, -- Vanilla
+	texture = 357, -- Faithful 32x
 }
 
