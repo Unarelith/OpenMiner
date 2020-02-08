b7bb8f1d52373ab0efd18cca33b84710aef55ccc
Quentin Bazin <quent42340@gmail.com>
2020-02-08 18:34:26 +0900

    Replaced my custom headers by LGPL license headers.

--- client/source/world/ClientWorld.cpp
+++ client/source/world/ClientWorld.cpp
@@ -1,13 +1,22 @@
 /*
  * =====================================================================================
  *
- *       Filename:  ClientWorld.cpp
+ *  OpenMiner
+ *  Copyright (C) 2018-2020 Unarelith, Quentin Bazin <openminer@unarelith.net>
  *
- *    Description:
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
  *
- *        Created:  12/01/2019 14:01:17
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
  *
- *         Author:  Quentin Bazin, <quent42340@gmail.com>
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
  *
  * =====================================================================================
  */
--- common/source/core/TilesDef.cpp
+++ common/source/core/TilesDef.cpp
@@ -1,13 +1,22 @@
 /*
  * =====================================================================================
  *
- *       Filename:  TilesDef.cpp
+ *  OpenMiner
+ *  Copyright (C) 2018-2020 Unarelith, Quentin Bazin <openminer@unarelith.net>
  *
- *    Description:
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
  *
- *        Created:  30/01/2020 17:27:16
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
  *
- *         Author:  Quentin Bazin, <quent42340@gmail.com>
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
  *
  * =====================================================================================
  */
