7bad35ed54d3194059ac9135ffd9524591b0afbe
Quentin Bazin <quent42340@gmail.com>
2020-02-11 20:53:03 +0900

    [SimplexNoise] Removed. [TerrainGeneration] Now using FastNoise for land height.

--- server/include/world/TerrainGenerator.hpp
+++ server/include/world/TerrainGenerator.hpp
@@ -38,17 +38,10 @@ class TerrainGenerator {
 		void setBlocksFromLuaTable(const sol::table &table);
 
 	private:
-		void lightTestGeneration(ServerChunk &chunk) const;
-		void basicGeneration(ServerChunk &chunk) const;
-		void testCraftGeneration(ServerChunk &chunk) const;
-		void simplexGeneration(ServerChunk &chunk) const;
 		void fastNoiseGeneration(ServerChunk &chunk) const;
 
 		static float noise2d(float x, float y, int octaves, float persistence);
 		static float noise3d_abs(float x, float y, float z, int octaves, float persistence);
-
-		static float newNoise2d(float x, float y, int octaves, float persistence);
-		static float newNoise3d_abs(float x, float y, float z, int octaves, float persistence);
 
 		u16 m_dirtBlockID = 0;
 		u16 m_grassBlockID = 0;
--- server/source/world/TerrainGenerator.cpp
+++ server/source/world/TerrainGenerator.cpp
@@ -28,18 +28,12 @@
 
 #include <glm/gtc/noise.hpp>
 #include "FastNoise.hpp"
-#include "SimplexNoise.hpp"
 
 TerrainGenerator::TerrainGenerator() {
-	srand(1337);
 }
 
 void TerrainGenerator::generate(ServerChunk &chunk) const {
-	// lightTestGeneration(chunk);
-	// basicGeneration(chunk);
-	testCraftGeneration(chunk);
-	// simplexGeneration(chunk);
-	// fastNoiseGeneration(chunk);
+	fastNoiseGeneration(chunk);
 }
 
 void TerrainGenerator::setBlocksFromLuaTable(const sol::table &table) {
@@ -53,66 +47,22 @@ void TerrainGenerator::setBlocksFromLuaTable(const sol::table &table) {
 	m_sandBlockID   = Registry::getInstance().getBlockFromStringID(table["sand"].get<std::string>()).id();
 }
 
-void TerrainGenerator::lightTestGeneration(ServerChunk &chunk) const {
-	for(u8 y = 0 ; y < CHUNK_HEIGHT ; y++) {
-		for(u8 z = 0 ; z < CHUNK_DEPTH ; z++) {
-			for(u8 x = 0 ; x < CHUNK_WIDTH ; x++) {
-				if (y == 4)
-					chunk.setBlockRaw(x, y, z, m_stoneBlockID);
-				else if (y == 10 && (z > 4 || chunk.z() != 0))
-					chunk.setBlockRaw(x, y, z, m_stoneBlockID);
-			}
-		}
-	}
+void TerrainGenerator::fastNoiseGeneration(ServerChunk &chunk) const {
+	FastNoise noise;
+	noise.SetNoiseType(FastNoise::NoiseType::SimplexFractal);
+	noise.SetFrequency(1 / 256.0f);
+	noise.SetFractalOctaves(4);
 
-	if (chunk.y() == 0) {
-		for(u8 z = 0 ; z < CHUNK_DEPTH ; z++) {
-			for(u8 x = 0 ; x < CHUNK_WIDTH ; x++) {
-				chunk.lightmap().addSunlight(x, 31, z, 15);
-			}
-		}
-	}
-
-	// Chunk *topChunk = chunk.getSurroundingChunk(Chunk::Top);
-	// if (topChunk) {
-	// 	for(u8 z = 0 ; z < CHUNK_DEPTH ; z++) {
-	// 		for(u8 x = 0 ; x < CHUNK_WIDTH ; x++) {
-	// 			int sunlightLevel = topChunk->lightmap().getSunlight(x, 0, z);
-	// 			if (sunlightLevel) {
-	// 				chunk.lightmap().addSunlight(x, CHUNK_HEIGHT - 1, z, sunlightLevel);
-	// 			}
-	// 		}
-	// 	}
-	// }
-}
-
-void TerrainGenerator::basicGeneration(ServerChunk &chunk) const {
-	for(u8 z = 0 ; z < CHUNK_DEPTH ; z++) {
-		for(u8 x = 0 ; x < CHUNK_WIDTH ; x++) {
-			float n = noise2d((x + chunk.x() * CHUNK_WIDTH) / 256.0, (z + chunk.z() * CHUNK_DEPTH) / 256.0, 5, 0.5) * 4;
-			float h = 10 + n * 2;
-
-			for(u8 y = 0 ; y < CHUNK_HEIGHT ; y++) {
-				if(y + chunk.y() * CHUNK_HEIGHT < h) {
-					chunk.setBlockRaw(x, y, z, 1);
-				}
-				else if (y == CHUNK_HEIGHT - 1) {
-					chunk.lightmap().addSunlight(x, y, z, 15);
-				}
-			}
-		}
-	}
-}
-
-void TerrainGenerator::testCraftGeneration(ServerChunk &chunk) const {
 	srand(chunk.x() + chunk.y() + chunk.z() + 1337);
 	Chunk *topChunk = chunk.getSurroundingChunk(Chunk::Top);
-
 	for(int z = 0 ; z < CHUNK_DEPTH ; z++) {
 		for(int x = 0 ; x < CHUNK_WIDTH ; x++) {
 			// Land height
-			float n = noise2d((x + chunk.x() * CHUNK_WIDTH) / 256.0, (z + chunk.z() * CHUNK_DEPTH) / 256.0, 4, 0.5) * 4;
-			float h = 10 + n * 2;
+			float n = noise.GetNoise(x + chunk.x() * CHUNK_WIDTH, z + chunk.z() * CHUNK_DEPTH);
+			float h = 10 + n * 20;
+
+			// float n = noise2d((x + chunk.x() * CHUNK_WIDTH) / 256.0, (z + chunk.z() * CHUNK_DEPTH) / 256.0, 4, 0.5) * 4;
+			// float h = 10 + n * 2;
 
 			// Land blocks
 			for(int y = 0 ; y < CHUNK_HEIGHT ; y++) {
@@ -151,20 +101,6 @@ void TerrainGenerator::testCraftGeneration(ServerChunk &chunk) const {
 					}
 				}
 				else {
-					// // Random value used to determine land type
-					// float r = noise3d_abs((x + chunk.x() * CHUNK_WIDTH) / 256.0, (y + chunk.y() * CHUNK_HEIGHT) / 256.0 + 16, (z + chunk.z() * CHUNK_DEPTH) / 256.0, 5, 0.5) * 4;
-                    //
-					// // Sand layer
-					// if(n * 4 + r * 5 < 4) {
-					// 	chunk.setBlockRaw(x, y, z, m_sandBlockID);
-					// }
-					// // Dirt layer, but use grass blocks for the top
-					// else if(n + r * 5 < 6 * 8 && n * 10 + r * 5 > 30) {
-					// 	chunk.setBlockRaw(x, y, z, (h < SEALEVEL - 5 || y + chunk.y() * CHUNK_HEIGHT < h - 1) ? m_dirtBlockID : m_grassBlockID);
-					// }
-					// else {
-					// 	chunk.setBlockRaw(x, y, z, m_stoneBlockID);
-					// }
 					if (y + chunk.y() * CHUNK_HEIGHT >= h - 1 && y + chunk.y() * CHUNK_HEIGHT > SEALEVEL - 1)
 						chunk.setBlockRaw(x, y, z, m_grassBlockID);
 					else if (y + chunk.y() * CHUNK_HEIGHT <= SEALEVEL - 1 && h < SEALEVEL && y + chunk.y() * CHUNK_HEIGHT > h - 3)
@@ -184,146 +120,6 @@ void TerrainGenerator::testCraftGeneration(ServerChunk &chunk) const {
 						chunk.setBlockRaw(x, y, z, 0);
 						chunk.setBlockRaw(x, y + 1, z, 0);
 					}
-				}
-
-				if (topChunk && topChunk->isInitialized()) {
-					int sunlightLevel = topChunk->lightmap().getSunlight(x, 0, z);
-					if (sunlightLevel) {
-						chunk.lightmap().addSunlight(x, CHUNK_HEIGHT - 1, z, sunlightLevel);
-					}
-				}
-			}
-		}
-	}
-}
-
-void TerrainGenerator::simplexGeneration(ServerChunk &chunk) const {
-	srand(1337);
-	Chunk *topChunk = chunk.getSurroundingChunk(Chunk::Top);
-	for(int z = 0 ; z < CHUNK_DEPTH ; z++) {
-		for(int x = 0 ; x < CHUNK_WIDTH ; x++) {
-			// Land height
-			float n = newNoise2d((x + chunk.x() * CHUNK_WIDTH) / 256.0, (z + chunk.z() * CHUNK_DEPTH) / 256.0, 4, 0.5) * 4;
-			float h = 10 + n * 2;
-
-			// Land blocks
-			for(int y = 0 ; y < CHUNK_HEIGHT ; y++) {
-				// Are we above "ground" level?
-				if(y + chunk.y() * CHUNK_HEIGHT > h) {
-					// If we are not yet up to sea level, fill with water blocks
-					if(y + chunk.y() * CHUNK_HEIGHT < SEALEVEL) {
-						chunk.setBlockRaw(x, y, z, m_waterBlockID);
-					}
-					// Otherwise we are in the air, so try to make a tree
-					else if(chunk.getBlock(x, y - 1, z) == m_grassBlockID && (rand() % 256) == 0 && n < 4) {
-						// Trunk
-						h = (rand() & 0x3) + 3;
-						for(int i = 0 ; i < h ; i++) {
-							chunk.setBlockRaw(x, y + i, z, m_logBlockID);
-						}
-
-						// Leaves
-						for(int ix = -3 ; ix <= 3 ; ix++) {
-							for(int iy = -3 ; iy <= 3 ; iy++) {
-								for(int iz = -3 ; iz <= 3 ; iz++) {
-									if(ix * ix + iy * iy + iz * iz < 8 + (rand() & 1) && !chunk.getBlock(x + ix, y + h + iy, z + iz)) {
-										chunk.setBlockRaw(x + ix, y + h + iy, z + iz, m_leavesBlockID);
-									}
-								}
-							}
-						}
-					}
-					// Or a flower
-					else if(chunk.getBlock(x, y - 1, z) == m_grassBlockID && (rand() & 0xff) == 0) {
-						chunk.setBlockRaw(x, y, z, m_flowerBlockID);
-					}
-					// If we are on the top block of the chunk, add sunlight
-					else if (y == CHUNK_HEIGHT - 1) {
-						chunk.lightmap().addSunlight(x, y, z, 15);
-					}
-				}
-				else {
-					if (y + chunk.y() * CHUNK_HEIGHT >= h - 1 && y + chunk.y() * CHUNK_HEIGHT > SEALEVEL - 1)
-						chunk.setBlockRaw(x, y, z, m_grassBlockID);
-					else if (y + chunk.y() * CHUNK_HEIGHT <= SEALEVEL - 1 && h < SEALEVEL && y + chunk.y() * CHUNK_HEIGHT > h - 3)
-						chunk.setBlockRaw(x, y, z, m_sandBlockID);
-					else if (y + chunk.y() * CHUNK_HEIGHT > h - 3)
-						chunk.setBlockRaw(x, y, z, m_dirtBlockID);
-					else
-						chunk.setBlockRaw(x, y, z, m_stoneBlockID);
-				}
-
-				if (topChunk && topChunk->isInitialized()) {
-					int sunlightLevel = topChunk->lightmap().getSunlight(x, 0, z);
-					if (sunlightLevel) {
-						chunk.lightmap().addSunlight(x, CHUNK_HEIGHT - 1, z, sunlightLevel);
-					}
-				}
-			}
-		}
-	}
-}
-
-void TerrainGenerator::fastNoiseGeneration(ServerChunk &chunk) const {
-	FastNoise noise;
-	noise.SetNoiseType(FastNoise::NoiseType::SimplexFractal);
-	noise.SetFrequency(1);
-	noise.SetFractalOctaves(4);
-
-	srand(1337);
-	Chunk *topChunk = chunk.getSurroundingChunk(Chunk::Top);
-	for(int z = 0 ; z < CHUNK_DEPTH ; z++) {
-		for(int x = 0 ; x < CHUNK_WIDTH ; x++) {
-			// Land height
-			float d = 256.0f;
-			float n = noise.GetNoise((x + chunk.x() * CHUNK_WIDTH) / d, (z + chunk.z() * CHUNK_DEPTH) / d);
-			float h = 10 + n * 20;
-
-			// Land blocks
-			for(int y = 0 ; y < CHUNK_HEIGHT ; y++) {
-				// Are we above "ground" level?
-				if(y + chunk.y() * CHUNK_HEIGHT > h) {
-					// If we are not yet up to sea level, fill with water blocks
-					if(y + chunk.y() * CHUNK_HEIGHT < SEALEVEL) {
-						chunk.setBlockRaw(x, y, z, m_waterBlockID);
-					}
-					// Otherwise we are in the air, so try to make a tree
-					else if(chunk.getBlock(x, y - 1, z) == m_grassBlockID && (rand() % 256) == 0 && n < 1) {
-						// Trunk
-						h = (rand() & 0x3) + 3;
-						for(int i = 0 ; i < h ; i++) {
-							chunk.setBlockRaw(x, y + i, z, m_logBlockID);
-						}
-
-						// Leaves
-						for(int ix = -3 ; ix <= 3 ; ix++) {
-							for(int iy = -3 ; iy <= 3 ; iy++) {
-								for(int iz = -3 ; iz <= 3 ; iz++) {
-									if(ix * ix + iy * iy + iz * iz < 8 + (rand() & 1) && !chunk.getBlock(x + ix, y + h + iy, z + iz)) {
-										chunk.setBlockRaw(x + ix, y + h + iy, z + iz, m_leavesBlockID);
-									}
-								}
-							}
-						}
-					}
-					// Or a flower
-					else if(chunk.getBlock(x, y - 1, z) == m_grassBlockID && (rand() & 0xff) == 0) {
-						chunk.setBlockRaw(x, y, z, m_flowerBlockID);
-					}
-					// If we are on the top block of the chunk, add sunlight
-					else if (y == CHUNK_HEIGHT - 1) {
-						chunk.lightmap().addSunlight(x, y, z, 15);
-					}
-				}
-				else {
-					if (y + chunk.y() * CHUNK_HEIGHT >= h - 1 && y + chunk.y() * CHUNK_HEIGHT > SEALEVEL - 1)
-						chunk.setBlockRaw(x, y, z, m_grassBlockID);
-					else if (y + chunk.y() * CHUNK_HEIGHT <= SEALEVEL - 1 && h < SEALEVEL && y + chunk.y() * CHUNK_HEIGHT > h - 3)
-						chunk.setBlockRaw(x, y, z, m_sandBlockID);
-					else if (y + chunk.y() * CHUNK_HEIGHT > h - 3)
-						chunk.setBlockRaw(x, y, z, m_dirtBlockID);
-					else
-						chunk.setBlockRaw(x, y, z, m_stoneBlockID);
 				}
 
 				if (topChunk && topChunk->isInitialized()) {
@@ -358,34 +154,6 @@ float TerrainGenerator::noise3d_abs(float x, float y, float z, int octaves, floa
 
 	for(int i = 0 ; i < octaves ; i++) {
 		sum += strength * fabs(glm::simplex(glm::vec3{x, y, z} * scale));
-		scale *= 2.0;
-		strength *= persistence;
-	}
-
-	return sum;
-}
-
-float TerrainGenerator::newNoise2d(float x, float y, int octaves, float persistence) {
-	float sum = 0;
-	float strength = 1.0;
-	float scale = 1.0;
-
-	for(int i = 0 ; i < octaves ; i++) {
-		sum += strength * SimplexNoise::noise(x * scale, y * scale);
-		scale *= 2.0;
-		strength *= persistence;
-	}
-
-	return sum;
-}
-
-float TerrainGenerator::newNoise3d_abs(float x, float y, float z, int octaves, float persistence) {
-	float sum = 0;
-	float strength = 1.0;
-	float scale = 1.0;
-
-	for(int i = 0 ; i < octaves ; i++) {
-		sum += strength * fabs(SimplexNoise::noise(x * scale, y * scale, z * scale));
 		scale *= 2.0;
 		strength *= persistence;
 	}
