1574fb67c309d28da9893b41507d882fa0a054fe
leonidomgz <leonidomgz@gmail.com>
2020-03-02 21:09:34 +0100

    final final final no more changes, we got this thing working

--- resources/shaders/game.f.glsl
+++ resources/shaders/game.f.glsl
@@ -48,19 +48,24 @@ void main() {
 	float minBrightness = 2.0 / 16.0;
 
 	if (lightCheck != -1.) {
+		float pi = 3.14159265358979323846264338327950288419716939937510; // useless? high precision pi stuff
+		//		the block is pretty much a super primitive version of the 3 lines after it
+		//		mostly due to deletboi not having any idea what clamp is and
+		//		having never worked in a professional environment
+		// float sunlight = v_lightValue.x;
+		// if (ticktime >=0 && ticktime <= 256000)
+		// {
+		// 	sunlight *= min(sin(ticktime/256000 + pi*u_time/256000), 1);
+		// 	// color = light(color, vec3(1.0, 1.0, 1.0), v_coord3d, ambientIntensity, diffuseIntensity);
+		// }
+		// else if(ticktime > 256000 && ticktime <512000)
+		// {
+		// 	sunlight *= max(cos(ticktime/256000 + pi*u_time/256000), 0);
+		// }
 
-		float ticktime = mod(u_time, 10000);
-		float pi = 3.14159265358979323846264338327950288419716939937510;
-		float sunlight = v_lightValue.x;
-		if (ticktime >=0 && ticktime <= 5000)
-		{
-			sunlight *= min(sin(ticktime/5000 + pi*u_time/5000), 1);
-			// color = light(color, vec3(1.0, 1.0, 1.0), v_coord3d, ambientIntensity, diffuseIntensity);
-		}
-		else if(ticktime > 5000 && ticktime <10000)
-		{
-			sunlight *= max(cos(ticktime/5000 + pi*u_time/5000), 0);
-		}
+		const float frequency = 256000;
+		float time = mod(u_time, 512000);
+		float sunlight = clamp(v_lightValue.x * 0.5 * (1 + sin(2 * pi * time / frequency)), 0, 15);
 
 		float ambientIntensity = max(max(sunlight, v_lightValue.y) / 16.0, minBrightness);
 		float diffuseIntensity = max(sunlight, v_lightValue.y) / 32.0;
