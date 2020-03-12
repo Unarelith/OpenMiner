f5a27cbd0a32a55d33c70198bbe507624a5d79ce
Quentin Bazin <quent42340@gmail.com>
2020-03-12 15:54:13 +0100

    [config.example.lua] Added.

---
+++ config.example.lua
@@ -0,0 +1,30 @@
+-- You can copy this file as 'config.lua' to load automatically those settings at client startup
+-- Here the default values are set, you can remove options or change them accordingly
+-- See client/source/Config.cpp for more details
+
+-- Gameplay
+isFlyModeEnabled = false
+isNoClipEnabled = false
+
+-- Interface
+isBlockInfoWidgetEnabled = true
+isFpsCounterEnabled = true
+isHotbarVisible = true
+isCrosshairVisible = true
+
+-- Graphics
+renderDistance = 8
+isTorchSmoothLightingEnabled = true
+isSunSmoothLightingEnabled = true
+isAmbientOcclusionEnabled = false
+isWireframeModeEnabled = false
+isFullscreenModeEnabled = false
+isVerticalSyncEnabled = true
+cameraFOV = 70.0
+screenWidth = 1600
+screenHeight = 1050
+guiScale = 3
+
+-- Input
+mouseSensitivity = 8
+
