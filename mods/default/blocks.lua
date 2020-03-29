c3eaa0665355df3d3acc7112f9b4a14d5ea8042c
Quentin Bazin <quent42340@gmail.com>
2020-03-29 17:55:15 +0200

    [blocks.lua] Cactus bottom texture added.

--- mods/default/blocks.lua
+++ mods/default/blocks.lua
@@ -245,7 +245,7 @@ mod:block {
 mod:block {
 	id = "cactus",
 	name = "Cactus",
-	tiles = {"cactus_top.png", "cactus_side.png"},
+	tiles = {"cactus_top.png", "cactus_bottom.png", "cactus_side.png"},
 
 	draw_type = "cactus",
 	bounding_box = {1/16, 1/16, 0, 14/16, 14/16, 1};
