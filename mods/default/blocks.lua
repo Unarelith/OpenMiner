52b53ebfe4db0c17320877c88724ad25dca84132
Quentin Bazin <quent42340@gmail.com>
2020-07-11 02:03:44 +0200

    [mods/default/blocks.lua] Removed old 'Redstone Lamp'.

--- mods/default/blocks.lua
+++ mods/default/blocks.lua
@@ -322,33 +322,6 @@ mod:block {
 }
 
 mod:block {
-	id = "redstone_lamp_off",
-	name = "Redstone Lamp",
-	tiles = "redstone_lamp_off.png",
-
-	on_block_activated = function(pos, block, player, world, client, server, screen_width, screen_height, gui_scale)
-		local block = openminer.registry:get_block_from_string("default:redstone_lamp_on")
-		world:set_block(pos.x, pos.y, pos.z, block:id())
-	end
-}
-
-mod:block {
-	id = "redstone_lamp_on",
-	name = "Redstone Lamp",
-	tiles = "redstone_lamp_on.png",
-	is_light_source = true,
-
-	groups = {
-		ci_ignore = 1
-	},
-
-	on_block_activated = function(pos, block, player, world, client, server, screen_width, screen_height, gui_scale)
-		local block = openminer.registry:get_block_from_string("default:redstone_lamp_off")
-		world:set_block(pos.x, pos.y, pos.z, block:id())
-	end
-}
-
-mod:block {
 	id = "farmland",
 	name = "Farmland",
 	tiles = {"farmland_dry.png", "dirt.png", "dirt.png"},
